7106a655e700bd612eb014335d325b0228c6df8f
80000061 <PanLei72>
2024-03-07 15:54:17 +0800

    first commit

---
+++ src/main/java/com/x/equipment/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.x.equipment.view.user;
+
+import com.x.equipment.entity.User;
+import com.x.equipment.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController("EQUI_User.list")
+@ViewDescriptor("user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
