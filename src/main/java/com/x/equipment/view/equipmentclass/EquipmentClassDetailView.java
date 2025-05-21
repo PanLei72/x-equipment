b7c881ca87957b218e22861a0770bdb76de9b282
water <panlei72@163.com>
2025-05-21 17:01:59 +0800

    设备类维护修改

--- src/main/java/com/x/equipment/view/equipmentclass/EquipmentClassDetailView.java
+++ src/main/java/com/x/equipment/view/equipmentclass/EquipmentClassDetailView.java
@@ -1,23 +1,15 @@
 package com.x.equipment.view.equipmentclass;
 
+import com.vaadin.flow.router.Route;
 import com.x.equipment.entity.EquipmentClass;
-
 import com.x.equipment.entity.EquipmentClassEquipment;
 import com.x.equipment.view.equipmentclassequipment.EquipmentClassEquipmentListView;
 import com.x.equipment.view.main.MainView;
-
-import com.vaadin.flow.router.Route;
 import io.jmix.flowui.DialogWindows;
 import io.jmix.flowui.component.grid.DataGrid;
 import io.jmix.flowui.kit.action.ActionPerformedEvent;
 import io.jmix.flowui.view.*;
-import io.jmix.securityflowui.model.BaseRoleModel;
-import io.jmix.securityflowui.view.resourcerole.ResourceRoleModelLookupView;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Route(value = "equipmentClasses/:id", layout = MainView.class)
 @ViewController("EQUI_EquipmentClass.detail")
